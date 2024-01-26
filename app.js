10c00f97ca657fa985eda12319f01a7e9b78a1d2
Mohib <mohibullahmohim2020@gmail.com>
2024-01-27 02:07:26 +0600

    Grade added.

--- app.js
+++ app.js
@@ -28,20 +28,55 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
-let hight_semi = parseFloat(prompt(`Enter Your correct Height in Inch:`));
-let height = parseFloat(hight_semi / 100) ;
-let weight = parseFloat(prompt(`Enter Your correct Weight:`));
-let BMI = parseFloat(weight / height**2 );
+function forBMIcheck(){
+    let hight_semi = parseFloat(prompt(`Enter Your correct Height in Inch:`));
+    let height = parseFloat(hight_semi / 100) ;
+    let weight = parseFloat(prompt(`Enter Your correct Weight:`));
+    let BMI = parseFloat(weight / height**2 );
 
-if(BMI < 18.5 ){
-    document.write(`Your are under weight. And Your BMI =` + BMI.toFixed(3));
+    if(BMI < 18.5 ){
+        document.write(`Your are under weight. And Your BMI =` + BMI.toFixed(3));
+    }
+    else if( BMI >= 18.5 && BMI <=24.9){
+        document.write(`You are Normal. And Your BMI =` + BMI.toFixed(3));    
+    }
+    else if(BMI >= 25){
+        document.write('You are Over Weight. And Your BMI =' + BMI.toFixed(3));
+    }
+    else{
+        document.write( 'You are obese. And Your BMI =' + BMI.toFixed(3));
+    }
 }
-else if( BMI >= 18.5 && BMI <=24.9){
-    document.write(`You are Normal. And Your BMI =` + BMI.toFixed(3));
-}
-else if(BMI >= 25){
-    document.write('You are Over Weight. And Your BMI =' + BMI.toFixed(3));
-}
-else{
-    document.write( 'You are obese. And Your BMI =' + BMI.toFixed(3));
+
+/***
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
+function forResult(){
+    let score = parseFloat(prompt(`Enter Your one sub Number :`));
+
+    if(score >= 90 && score <= 100){
+        document.write(`Your grade in this subject : A` );
+    }
+    else if(score >= 80 && score <= 89){
+        document.write(`Your grade in this subject : B` );
+    }
+    else if(score >= 70 && score <= 79){
+        document.write(`Your grade in this subject : c` );
+    }
+    else if(score >= 60 && score <= 69){
+        document.write(`Your grade in this subject : D` );
+    }
+    else if(score >= 0 && score <=59){
+        document.write(`Your grade in this subject : F` );
+    }
+    else if(score < 0 || score > 100){
+        document.write(`Your Number is invalid.`);
+    }
 }
